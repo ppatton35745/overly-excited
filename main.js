1679b132292b72dcc1f0a9db108ac13f0434b387
Philip Patton <ppatton35745@gmail.com>
2018-05-24 13:35:20 -0500

    Beautified the file, wooo

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
-let aBetterSentence = ["This","is","one","of","the","BEST","sentences","that","I","have","ever","written"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+let aBetterSentence = ["This", "is", "one", "of", "the", "BEST", "sentences", "that", "I", "have", "ever", "written"];
 
 let addExcitement = (sentence, punctuation) => {
     let buildMeUp = ""
 
     for (let i = 1; i <= sentence.length; i++) {
-        if(i % 3 !== 0) {
+        if (i % 3 !== 0) {
             buildMeUp += (`${sentence[i - 1]} `);
-        }
-        else {
+        } else {
             let exclaimations = ""
             let j = i / 3;
-            
+
             for (k = 0; k < j; k++) {
                 exclaimations += punctuation;
             }
-            
+
             buildMeUp += (`${sentence[i - 1]}${exclaimations} `);
         }
-       
+
         console.log(buildMeUp);
     }
 
